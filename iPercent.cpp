514544ebf56e2401641fab667240d36e2ac55b3a
ShreyaDate <shreya.msoxford@gmail.com>
2014-08-06 08:36:13 +0530

    Create iPercent.cpp
    Percentage Calculation

---
+++ iPercent.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int marks = 220;
+  int total = 250;
+  int percent;
+  
+  percent = (marks/total)*100;
+  
+  cout<<"\nPercentage obtained: "<<percent;
+  
+  return 0;
+}
